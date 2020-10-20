66990fe18d459badb6116a48ccfb8cf813b92239
kamal <kamal@xoxzo.com>
2020-10-20 16:27:53 +0800

    add chat

--- vuepony/fe/js/app.js
+++ vuepony/fe/js/app.js
@@ -6,6 +6,10 @@ Vue.component(
   "demo-component",
   require("./components/DemoComponent.vue").default
 );
+Vue.component(
+  "chat-component",
+  require("./components/ChatComponent.vue").default
+);
 
 let vue = new Vue({
   //
